ed6f8cad1287510f72a57a53a8478eb410fc3cf6
alefiyaV <alefiyavahanvaty@gmail.com>
2018-10-20 15:38:52 -0700

    onChange tested and reading the date correctly

--- client/src/components/Calendar.js
+++ client/src/components/Calendar.js
@@ -1,19 +1,26 @@
 import React, { Component } from 'react'
-import { Container, Button } from 'semantic-ui-react'
-import { Link } from 'react-router-dom'
+//import { Link } from 'react-router-dom'
 import Reactcal from 'react-calendar'
 
 class Calendar extends Component {
   state = {
-    date: new Date(),
+    date: new Date()
   }
   
+  onChange = date => this.setState({ date })
+  
   render () {
-    return <div className="row">
+    console.log(this.state.date)
+    return (<div className="row">
     <div className="main col-8">
       <h1>Calendar Page</h1>
+      <Reactcal 
+      onChange={this.onChange}
+      value={this.state.date}
+       />
 </div>
 </div>
+    )
   }
 }
 
