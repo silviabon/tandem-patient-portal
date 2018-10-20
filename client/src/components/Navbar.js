8cb8ed3df4cb906291cd53b5a82f6a19b68e0412
bguids91 <bguids91@gmail.com>
2018-10-20 14:41:52 -0700

    Add logout button

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -3,9 +3,10 @@ import React, { Component } from 'react'
 class Navbar extends Component {
   render () {
     return(
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-  <a className="navbar-brand" href="#">Navbar</a>
-</nav>)
+    <nav className="navbar navbar-light bg-light">
+      <a className="navbar-brand" href="#">Navbar</a>
+      <button className="btn btn-danger navbar-btn"> Logout </button>
+    </nav>)
   }
 }
 
