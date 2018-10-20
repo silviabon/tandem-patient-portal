6427c65b39bdf5fcbe2dd783cc55843828cb37dc
bguids91 <bguids91@gmail.com>
2018-10-19 21:30:02 -0700

    Adds bootstrap and navbar

--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import App from './App'
+import 'bootstrap/dist/css/bootstrap.min.css';
 import 'semantic-ui-css/semantic.css'
 import './css/index.css'
 
