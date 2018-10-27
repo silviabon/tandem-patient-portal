dc360e8f5fb5bff53da93d99b8f7be3ee811eff1
bguids91 <bguids91@gmail.com>
2018-10-26 23:03:21 -0700

    Adds css styling

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -12,7 +12,7 @@ class Navbar extends Component {
     <nav className="navbar sticky-top navbar-light">
     <a className='nav-brand' href='/home'>
     <img id="logo" src='images/logo_clipped_rev_3.png' width="70" height="70" class="d-inline-block align-top" alt="" />
-    tandem health
+    <b>tandem</b> health portal
     </a>
       {patient
           ? (<span className='nav-name justify-content-end'>
