d6d60026bb97aac58c82cd13e0812f1428ef14ee
Silvia Bon <bon.silvia@gmail.com>
2018-10-29 12:02:01 -0700

    fixed dates

--- client/src/components/Prescription.jsx
+++ client/src/components/Prescription.jsx
@@ -7,14 +7,14 @@ class Prescription extends Component {
 
     return (
       <div className='card-body med-info'>
-      <p><em>{prescription.name}</em>< br/>
-      <span>Dose:</span><span>{prescription.dose}</span>< br/>
-      <span>Refill:</span><span>{prescription.refill}</span>< br/>
-      <span>Route:</span><span>{prescription.route}</span>< br/>
-      <span>Date:</span><span>{prescription.date}</span></p>
+        <p><em>{prescription.name}</em>< br />
+          <span>Dose:</span><span>{prescription.dose}</span>< br />
+          <span>Refill:</span><span>{prescription.refill}</span>< br />
+          <span>Route:</span><span>{prescription.route}</span>< br />
+          <span>Date:</span><span>{prescription.date}</span></p>
       </div>
     );
   }
 }
 
-export default Prescription;
+export default Prescription
